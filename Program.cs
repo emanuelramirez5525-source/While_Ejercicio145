75fa47c2c1e2272a6ac7df88ef0970cda18b07a0
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 16:46:24 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿// Sumar los números del 1 al 100.
+
+int contador = 1;
+int suma = 0;
+
+while (contador <= 100)
+{
+    suma += contador;
+    contador++;
+}
+Console.WriteLine("La suma es = "+ suma);
